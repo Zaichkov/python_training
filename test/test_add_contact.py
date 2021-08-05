98a7611e79f830cafa8f3149509e698b8096a235
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2021-08-05 22:33:23 +0300

    Выделен класс для работы с контактами

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,8 +13,8 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Sergey", lastname="Zaichkov", address="Gomel",
-                                        email="blabla@gmail.com", mobile_phone="12345678",
-                                        bday="20", bmonth="September", byear="1987",
-                                        aday="14", amonth="June", ayear="2021"))
+    app.contact.create(Contact(firstname="Sergey", lastname="Zaichkov", address="Gomel",
+                               email="blabla@gmail.com", mobile_phone="12345678",
+                               bday="20", bmonth="September", byear="1987",
+                               aday="14", amonth="June", ayear="2021"))
     app.session.logout()
