36dd3ee173a32634f83d4715c04776d399544dd8
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2021-08-06 18:38:37 +0300

    Добавлен тест удаления первого контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -98,3 +98,10 @@ class ContactHelper:
         # submit contact creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.app.open_home_page()
+
+    def delete_first_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_css_selector("input[value='Delete']").click()
+        wd.switch_to_alert().accept()
+        self.app.open_home_page()
