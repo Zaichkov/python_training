742ee29429c35a355699a9e60e70ee3f75770132
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2021-08-10 08:24:18 +0300

    Добавлены проверки валидности фикстуры и состояния сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,9 +3,7 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Sergey", lastname="Zaichkov", address="Gomel",
                                email="blabla@gmail.com", mobile_phone="12345678",
                                bday="20", bmonth="September", byear="1987",
                                aday="14", amonth="June", ayear="2021"))
-    app.session.logout()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,8 +2,6 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="edited_firstname", lastname="edited_lastname",
                                            address="edited_address", mobile_phone="edited_phone", email="edited_email",
                                            title="new_title", bday="19", bmonth="October", byear="1988"))
-    app.session.logout()
