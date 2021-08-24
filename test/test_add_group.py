6d81cfabc32b34ca2ddd4d507a8f032a9c84b0cc
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2021-08-24 23:17:57 +0300

    Проверки путём сравнения списков в тестах для создания, удаления и модификации групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,4 +2,10 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="myGroup", header="header"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="myGroup", header="header")
+    app.group.create(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,4 +4,10 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
-    app.group.edit_first_group(Group(name="super_group", footer="fooooooter"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="super_group", footer="fooooooter", id=old_groups[0].id)
+    app.group.edit_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
